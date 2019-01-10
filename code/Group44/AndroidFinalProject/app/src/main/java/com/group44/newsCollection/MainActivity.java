aa4263b784c62780a6cd694f6fc4822161a95e00
z1wu <2682731305@qq.com>
2019-01-10 17:09:15 +0800

    basic left menu part

--- code/Group44/AndroidFinalProject/app/src/main/java/com/group44/newsCollection/MainActivity.java
+++ code/Group44/AndroidFinalProject/app/src/main/java/com/group44/newsCollection/MainActivity.java
@@ -6,7 +6,13 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.group44.newsCollection.fragment.ContentFragment;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
@@ -28,8 +34,42 @@ public class MainActivity extends SlidingFragmentActivity {
 
         setBehindContentView(R.layout.left_menu);
         SlidingMenu slidingMenu = getSlidingMenu();
+
         slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);//全屏触摸
         slidingMenu.setBehindOffset(450);//屏幕预留100像素宽度
+
+        // init sliding menu function
+        Switch settingNightMode = slidingMenu.findViewById(R.id.lm_s_night_mode);
+        LinearLayout allNewsGroup = slidingMenu.findViewById(R.id.lm_g_all_news);
+        LinearLayout recommendNews = slidingMenu.findViewById(R.id.lm_g_recommend_news);
+        LinearLayout settingTextSize = slidingMenu.findViewById(R.id.lm_g_text_size);
+
+
+        // 页面跳转逻辑
+        allNewsGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // navigate to all news panel
+                Toast.makeText(MainActivity.this, "all news", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        recommendNews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // navigate to recommend news panel
+                Toast.makeText(MainActivity.this, "推荐", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        settingTextSize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // change font size
+                Toast.makeText(MainActivity.this, "更改字体设置", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         initFragment();
     }
