5d44a9e8ff3bee7ff1f0972131f65f58aa995e6b
chensh236 <1466196675@qq.com>
2019-01-12 15:17:16 +0800

    add wordPress framework

---
+++ code/Group44/finalfinalProject/app/src/main/java/com/example/group44/newscollection/String_val.java
@@ -0,0 +1,11 @@
+package com.example.group44.newscollection;
+
+// 用于统计频数
+public class String_val {
+    public String chara;
+    public Integer val;
+    public String_val(String chara, Integer count){
+        this.chara = chara;
+        this.val = count;
+    }
+}
