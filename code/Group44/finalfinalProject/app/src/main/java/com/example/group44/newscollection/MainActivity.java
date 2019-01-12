417a6f8e4f96001bcbcc510f25a09e18a9d248b5
chensh236 <1466196675@qq.com>
2019-01-12 18:52:07 +0800

    add dislike tag

--- code/Group44/finalfinalProject/app/src/main/java/com/example/group44/newscollection/MainActivity.java
+++ code/Group44/finalfinalProject/app/src/main/java/com/example/group44/newscollection/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.group44.newscollection;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -78,6 +79,9 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, NewsDetail.class);
+                startActivity(intent);
+                finish();
             }
         });
 
